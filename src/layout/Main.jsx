992724d8eb942fc926aeaae16799dee9b1285c08
Sayem92 <sayemmiha123@gmail.com>
2024-01-31 19:58:27 +0600

    add footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const Main = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
