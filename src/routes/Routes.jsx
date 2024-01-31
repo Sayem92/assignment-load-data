7f90d59a07382e53f7893cc796fd58eceacb9e6d
Sayem92 <sayemmiha123@gmail.com>
2024-01-31 22:17:27 +0600

    add errorapge

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,15 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../components/home/Home";
+import ErrorPage from "../components/Errorpage/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <ErrorPage></ErrorPage>,
     element: <Main></Main>,
 
     children: [
       {
         path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/home",
         element: <Home></Home>,
       },
     ],
